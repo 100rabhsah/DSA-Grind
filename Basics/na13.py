de896fee7fdc4874300aa62450cffcc6608d7702
100rabhsah <sourabhsah31@gmail.com>
2025-02-06 22:14:57 +0530

    na13 done

---
+++ Basics/na13.py
@@ -0,0 +1,11 @@
+#Program for Decimal to Binary Conversion
+
+def dtob(n):
+    binary=""
+    while n>0:
+        binary=str(n%2)+binary
+        n//=2
+    return binary if binary else "0"
+
+n = int(input("Enter the decimal number to convert: "))
+print("The binary form is: ",dtob(n))
