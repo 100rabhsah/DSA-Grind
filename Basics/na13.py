a00f05c2ea261e0d53ece160d381161517df230b
100rabhsah <sourabhsah31@gmail.com>
2025-02-06 22:18:13 +0530

    na13 2nd method defined

--- Basics/na13.py
+++ Basics/na13.py
@@ -5,7 +5,12 @@ def dtob(n):
     while n>0:
         binary=str(n%2)+binary
         n//=2
-    return binary if binary else "0"
+    if binary:
+        return binary
+    else:
+        "0"
+
+    #return binary if binary else "0"
 
 n = int(input("Enter the decimal number to convert: "))
 print("The binary form is: ",dtob(n))
