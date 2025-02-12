fc46ed5e0829babc604e57290d221b29cde3fc68
100rabhsah <sourabhsah31@gmail.com>
2025-02-12 11:07:19 +0530

    sn32 done

--- Basics/sn32.py
+++ Basics/sn32.py
@@ -1,2 +1,16 @@
-#perfect number
+#perfect number- sum of its positive divisors excluding the number itself
 
+def perfect(n):
+    sum_divisors = 0
+
+    for i in range(1, n):  
+        if n % i == 0:  
+            sum_divisors += i  
+
+    if sum_divisors == n:  
+        print(True)  
+    else:  
+        print(False)
+
+n = int(input("Enter number n to check if perfect or not: "))
+perfect(n)   
