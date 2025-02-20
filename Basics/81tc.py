57c6f42b6b29c1ae2e2b21e8d9008ccf39e473dc
100rabhsah <sourabhsah31@gmail.com>
2025-02-20 13:29:18 +0530

    81tc done

---
+++ Basics/81tc.py
@@ -0,0 +1,5 @@
+#Program for converting hours into minutes and seconds
+
+hours=int(input("Enter hours: "))
+
+print(f"The conversion in minutes and seconds is: {hours*60}minutes and {hours*60*60}seconds")
